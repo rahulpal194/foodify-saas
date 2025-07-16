ccc29553ecc3a8639d5a0cb5138ee44787e1652f
Rahul Paul <168732280+rahulpal194@users.noreply.github.com>
2025-07-16 18:08:31 +0600

    update review, faq, pricing plan, login, signup

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ droodownbtn.forEach(btn=>{
         })
     })
 })
+
+
+const accordionBtn = document.querySelectorAll('.accordionBtn');
+accordionBtn.forEach((btn) => {
+    btn.addEventListener('click', function(event) {
+        const content = btn.lastElementChild;
+        const icon = btn.querySelector('.accordion-icon');
+        const expanded = btn.getAttribute('aria-expanded') === 'true';
+        accordionBtn.forEach((otherBtn) => {
+            const otherContent = otherBtn.lastElementChild;
+            const otherIcon = otherBtn.querySelector('.accordion-icon');
+            if (otherBtn !== btn) {
+                otherContent.style.height = '0px';
+                otherIcon.classList.remove('rotate-180');
+                otherContent.classList.remove('mt-6');
+                otherBtn.setAttribute('aria-expanded', 'false');
+            }
+        });
+        if (expanded) {
+            content.style.height = '0px';
+            icon.classList.remove('rotate-180');
+            content.classList.remove('mt-6');
+        } else {
+            content.style.height = `${content.scrollHeight}px`;
+            icon.classList.add('rotate-180');
+            content.classList.add('mt-6');
+        }
+        btn.setAttribute('aria-expanded', !expanded);
+    });
+});
